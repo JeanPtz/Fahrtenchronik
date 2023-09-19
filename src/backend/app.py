d4996c2e32dac360671c6ddccd11bcb19a972231
Jean Pütz <jean-adrian.puetz@gdata.de>
2023-09-19 14:05:04 +0200

    fixed old naming

--- src/backend/app.py
+++ src/backend/app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
-from RoutesRepository import RoutesRepository
+from GPXRepository import GPXRepository
 
 # Create a Flask application instance
 app = Flask(__name__)
@@ -11,7 +11,7 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 @app.route('/create-routes', methods=['POST'])
 def create_route():
     try:
-        routesRepository.create_routes()
+        gpxRepository.create_routes()
         return (jsonify({'message': 'Routes created successfully'}), 200)
     except Exception as e:
         return (jsonify({'error': str(e)}), 500)
@@ -19,7 +19,7 @@ def create_route():
 @app.route('/get-routes', methods=['GET'])
 def get_route():
     route_data = request.get_json()
-    requestedRoutes = routesRepository.get_routes(route_data['name'], route_data['kfz'], route_data['start_date'], route_data['end_date'], )
+    requestedRoutes = gpxRepository.get_routes(route_data['name'], route_data['kfz'], route_data['start_date'], route_data['end_date'], )
     return requestedRoutes
 
 if __name__ == '__main__':
@@ -28,5 +28,5 @@ if __name__ == '__main__':
         db_dir = os.path.dirname(database)
         if not os.path.exists(db_dir):
             os.makedirs(db_dir)
-    routesRepository = RoutesRepository(database)
+    gpxRepository = GPXRepository(database)
     app.run(debug=True) # Run the Flask application
